efff9a07b51906b32d33496f6a2e4d74cf4dc3f0
vadimboyko <vadim.boyko.01@outlook.com>
2026-04-08 01:18:12 +0300

    updated config

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express, { type Request, type Response, Router, urlencoded } from 'expres
 // import subscriptionsRouter from './routes/subscription';
 //
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!1')
